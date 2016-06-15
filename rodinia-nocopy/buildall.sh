0fa16474eb004a091b9207966c84fdec20aa1b84
Adarsh Patil <adarsh.patil@csa.iisc.ernet.in>
2016-06-15 16:50:05 +0530

    arth heartwall clean fix; buildall make/make gem-fusion status separate

--- rodinia-nocopy/buildall.sh
+++ rodinia-nocopy/buildall.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-results=()
+results1=()
+results2=()
 for i in backprop bfs cfd gaussian heartwall hotspot kmeans lavaMD leukocyte lud mummergpu nn nw particlefilter pathfinder srad streamcluster
 do
 	cd $i
 	make
+	results1=("${results1[@]}" "$? - $i")
 	make gem5-fusion
-	results=("${results[@]}" "$? - $i")
+	results2=("${results2[@]}" "$? - $i")
 	cd ..
 done
 cd particlefilter
@@ -15,5 +17,8 @@ results=("${results[@]}" "$? - particlefilter_float")
 mv gem5_fusion_particlefilter_naive.backup gem5_fusion_particlefilter_naive
 cd ..
 echo ""
-echo "Compilation status of benchmarks (0 : Success)"
-printf '%s\n' "${results[@]}"
+echo "make status of benchmarks (0 : Success)"
+printf '%s\n' "${results1[@]}"
+echo ""
+echo "make gem5-fusion status of benchmarks (0: Success)"
+printf '%s\n' "${results2[@]}"
